735859a27e2f462d5cab46893a11c5f17dc54d79
Nitsan Avni <nitsan@papayaglobal.com>
2019-03-05 10:53:41 +0200

    outline2list

---
+++ src/main/groovy/Outline2List.groovy
@@ -0,0 +1,40 @@
+def prev
+def parents = []
+def level = 0
+def output
+def prevOutput
+
+System.in.newReader()
+        .readLines()
+        .findAll { !it.trim().empty }
+        .collect {
+    def selfLevel = it.indexOf(it.trim())
+
+    def doOuput = true
+
+    if (selfLevel > level) {
+        level = selfLevel
+
+        parents << prev
+
+        doOuput = false
+    }
+
+    if (selfLevel < level) {
+        level = selfLevel
+
+        parents.pop()
+    }
+
+    prev = it
+
+    output = prevOutput
+
+    prevOutput = parents.join() + it
+
+    doOuput ? output : null
+}
+.minus(null)
+        .each { String line ->println line.replaceAll(/\s\s+/, " ") }
+
+println prevOutput
